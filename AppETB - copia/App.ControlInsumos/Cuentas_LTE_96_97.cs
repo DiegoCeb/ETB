f575bd582497ce626e28739839f7e364f3a947fd
g18leonardo <93055513+g18leonardo@users.noreply.github.com>
2022-09-14 18:14:53 -0500

    14-09-2022-18-14 BC
    Commiit para fusionar

---
+++ AppETB - copia/App.ControlInsumos/Cuentas_LTE_96_97.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.ControlInsumos
+{
+    public class Cuentas_LTE_96_97
+    {
+        public string Cruce { get; set; }
+        public string Resultados { get; set; }
+    }
+}
---
+++ AppETB - copia/AppETB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using App.ControlInsumos;
+using App.ControlProcesos;
+
+namespace AppETB
+{
+    /// <summary>
+    /// Clase Program
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Main
+            using (GestionProcesos objProcesos = new GestionProcesos())
+            {
+                Helpers.EscribirLogUsuario(Environment.UserName);
+                objProcesos.Menu();
+            }
+            #endregion
+        }
+       
+    }
+}
