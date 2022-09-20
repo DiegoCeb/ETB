c8a72c5a96ef2937f992a0b6a740b447d8a06fd0
Ivanm2b <92653855+Ivanm2b@users.noreply.github.com>
2022-09-20 18:37:41 -0500

    20-09-2022-18-37 IM
    Métodos de Mapeo Asignados

--- AppETB/App.ControlLogicaProcesos/ProcesoMasivos.cs
+++ AppETB/App.ControlLogicaProcesos/ProcesoMasivos.cs
@@ -55,5 +55,278 @@ namespace App.ControlLogicaProcesos
             //var datos = Helpers.CargueDinamicoInsumos<App.Variables.EstructuraExclusionSA>(App.Variables.Insumos.ExcluirServiciosAdicionales, datosInsumoServicioExclusion.RutaInsumo, , new object[] { "" ,"" ,"" })
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1AAA
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1AAA(List<string> datosOriginales)
+        {
+            #region MapeoCanal1AAA
+            string Linea1AAA = string.Empty;
+            return Linea1AAA; 
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1BBB
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public List<string> MapeoCanal1BBB(List<string> datosOriginales)
+        {
+            #region MapeoCanal1BBB
+            List<string> Lineas1BBB = new List<string>();
+            return Lineas1BBB; 
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1CCA
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1CCA(List<string> datosOriginales)
+        {
+            #region MapeoCanal1CCA
+            string Linea1CCA = string.Empty;
+            return Linea1CCA;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1CPA
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1CPA(List<string> datosOriginales)
+        {
+            #region MapeoCanal1CPA
+            string Linea1CPA = string.Empty;
+            return Linea1CPA;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1AFI
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1AFI(List<string> datosOriginales)
+        {
+            #region MapeoCanal1AFI
+            string Linea1AFI = string.Empty;
+            return Linea1AFI;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1BFI
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public List<string> MapeoCanal1BFI(List<string> datosOriginales)
+        {
+            #region MapeoCanal1BFI
+            List<string> Lineas1BFI = new List<string>();
+            return Lineas1BFI;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1CFI
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public List<string> MapeoCanal1CFI(List<string> datosOriginales)
+        {
+            #region MapeoCanal1CFI
+            List<string> Lineas1CFI = new List<string>();
+            return Lineas1CFI;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1TFI
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1TFI(List<string> datosOriginales)
+        {
+            #region MapeoCanal1TFI
+            string Linea1TFI = string.Empty;
+            return Linea1TFI;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1CDP
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1CDP(List<string> datosOriginales)
+        {
+            #region MapeoCanal1CDP
+            string Linea1CDP = string.Empty;
+            return Linea1CDP;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal CART
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public List<string> MapeoCanalCART(List<string> datosOriginales)
+        {
+            #region MapeoCanalCART
+            List<string> LineasCART = new List<string>();
+            return LineasCART;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1OMV
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public List<string> MapeoCanal1OMV(List<string> datosOriginales)
+        {
+            #region MapeoCanal1OMV
+            List<string> Lineas1OMV = new List<string>();
+            return Lineas1OMV;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal CONS
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public List<string> MapeoCanalCONS(List<string> datosOriginales)
+        {
+            #region MapeoCanalCONS
+            List<string> LineasCONS = new List<string>();
+            return LineasCONS;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal NTC5
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public List<string> MapeoCanalNTC5(List<string> datosOriginales)
+        {
+            #region MapeoCanalNTC5
+            List<string> LineasNTC5 = new List<string>();
+            return LineasNTC5;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1OOA
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1OOA(List<string> datosOriginales)
+        {
+            #region MapeoCanal1OOA
+            string Linea1OOA = string.Empty;
+            return Linea1OOA;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1HIS
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1HIS(List<string> datosOriginales)
+        {
+            #region MapeoCanal1HIS
+            string Linea1HIS = string.Empty;
+            return Linea1HIS;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene la linea formateada de Canal 1HDT
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1HDT(List<string> datosOriginales)
+        {
+            #region MapeoCanal1HDT
+            string Linea1HDT = string.Empty;
+            return Linea1HDT;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1DET
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1DET(List<string> datosOriginales)
+        {
+            #region MapeoCanal1DET
+            string Lineas1DET = string.Empty;
+            return Lineas1DET;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1LLL
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1LLL(List<string> datosOriginales)
+        {
+            #region MapeoCanal1LLL
+            string Lineas1LLL = string.Empty;
+            return Lineas1LLL;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1CIC
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1CIC(List<string> datosOriginales)
+        {
+            #region MapeoCanal1CIC
+            string Lineas1CIC = string.Empty;
+            return Lineas1CIC;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1RRT
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1RRT(List<string> datosOriginales)
+        {
+            #region MapeoCanal1RRT
+            string Lineas1RRT = string.Empty;
+            return Lineas1RRT;
+            #endregion
+        }
+
+        /// <summary>
+        /// Metodo que obtiene las lineas formateadas de Canal 1RRR
+        /// </summary>
+        /// <param name="datosOriginales"></param>
+        /// <returns></returns>
+        public string MapeoCanal1RRR(List<string> datosOriginales)
+        {
+            #region MapeoCanal1RRR
+            string Lineas1RRR = string.Empty;
+            return Lineas1RRR;
+            #endregion
+        }
     }
 }
